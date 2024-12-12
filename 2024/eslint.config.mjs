455f98087da190a490a78e40e1dd960820080efd
sonikblue <sonikblue@users.noreply.github.com>
2024-12-12 20:54:41 +0000

    Install eslint-config-prettier

--- 2024/eslint.config.mjs
+++ 2024/eslint.config.mjs
@@ -1,12 +1,13 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
 import tseslint from "typescript-eslint";
-
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintConfigPrettier,
 ];
